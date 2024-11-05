d6e48bda3e9fb3db728f97075e3622dfd380cb3b
최은 <nekoko83@gmail.com>
2024-11-05 22:47:18 +0900

    헤더 진행중

--- html/js/js_src/re_common.js
+++ html/js/js_src/re_common.js
@@ -1,4 +1,35 @@
 $(document).ready(function () {
+  // web
+  $('.re_gnb > .inner_wrap > .flex_wrap > .gnb > li').on('mouseover click', function () {
+    $('.re_gnb').addClass('on');
+
+    // return false;
+  });
+  $('.re_gnb > .inner_wrap > .flex_wrap > .gnb > li').mouseleave(function () {
+    $('.re_gnb').removeClass('on');
+  });
+
+
+  const menuItems = document.querySelectorAll('.re_gnb > .inner_wrap > .flex_wrap > .gnb > li');
+
+  menuItems.forEach(item => {
+    item.addEventListener('mouseenter', function () {
+      this.classList.add('on');
+    });
+
+    item.addEventListener('mouseleave', function () {
+      this.classList.remove('on');
+    });
+
+    item.addEventListener('click', function () {
+      this.classList.add('on');
+    });
+  });
+
+
+
+
+  
   // top_banner
   $('.top_banner_slider').slick({
     infinite: true,
